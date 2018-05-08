920614a86e66d8566fdadb1dcbbc988c86ba2143
nehalem518 <sonicmaverick@163.com>
2018-05-08 17:18:57 +0800

    test3

--- No.0001.Two_Sum.cpp
+++ No.0001.Two_Sum.cpp
@@ -9,11 +9,7 @@ public:
 		vector<int> result;
 		result.clear();
 		for (int i = 0; i != nums.size(); ++i) {
-			//if (nums[i] > target)
-			//	continue;
 			for (int j = i + 1; j != nums.size(); ++j) {
-				//if (nums[j] > target)
-				//	continue;
 				if ((nums[i] + nums[j]) == target) {
 					result.push_back(i);
 					result.push_back(j);
