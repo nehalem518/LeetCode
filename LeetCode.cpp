b346c7b1084083b71e49ba5c90193a226fb3d8ed
nehalem518 <sonicmaverick@163.com>
2018-05-08 16:57:21 +0800

    1st problem commit - Two Sum

---
+++ LeetCode.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> result;
+		result.clear();
+		for (int i = 0; i != nums.size(); ++i) {
+			//if (nums[i] > target)
+			//	continue;
+			for (int j = i + 1; j != nums.size(); ++j) {
+				//if (nums[j] > target)
+				//	continue;
+				if ((nums[i] + nums[j]) == target) {
+					result.push_back(i);
+					result.push_back(j);
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+};
+
+
+//int main()
+//{
+//	vector<int> ivec = { 0, 4, 3, 0 };
+//	Solution s;
+//	vector<int> result = s.twoSum(ivec, 0);
+//}
