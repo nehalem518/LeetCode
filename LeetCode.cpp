768d9a928ce28df24db9594e1bb3656c7d8e39de
nehalem518 <sonicmaverick@163.com>
2018-05-18 16:49:18 +0800

    add method 2: double pointer

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -9,11 +9,7 @@ public:
 		vector<int> result;
 		result.clear();
 		for (int i = 0; i != nums.size(); ++i) {
-			//if (nums[i] > target)
-			//	continue;
 			for (int j = i + 1; j != nums.size(); ++j) {
-				//if (nums[j] > target)
-				//	continue;
 				if ((nums[i] + nums[j]) == target) {
 					result.push_back(i);
 					result.push_back(j);
@@ -25,3 +21,10 @@ public:
 	}
 };
 
+
+//int main()
+//{
+//	vector<int> ivec = { 0, 4, 3, 0 };
+//	Solution s;
+//	vector<int> result = s.twoSum(ivec, 0);
+//}
