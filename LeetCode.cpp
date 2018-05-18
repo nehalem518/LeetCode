b3f7f37bf0911f97d99c1c5c8cdf7b1b6a6571a4
nehalem518 <sonicmaverick@163.com>
2018-05-18 17:37:34 +0800

    method V2: double pointer - AC

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,30 +1,61 @@
 #include <vector>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 class Solution
 {
+	struct CNum{
+		int num;
+		int index;
+	};
+
+	static int cmp(CNum a, CNum b)
+	{
+		return a.num < b.num;
+	}
+
 public:
 	vector<int> twoSum(vector<int>& nums, int target)
 	{
 		vector<int> result;
 		result.clear();
-		for (int i = 0; i != nums.size(); ++i) {
-			for (int j = i + 1; j != nums.size(); ++j) {
-				if ((nums[i] + nums[j]) == target) {
-					result.push_back(i);
-					result.push_back(j);
-					return result;
-				}
+
+		vector<CNum> vecNumbers;
+		for (auto i = 0; i != nums.size(); ++i) {
+			CNum temp;
+			temp.index = i;
+			temp.num = nums[i];
+			vecNumbers.push_back(temp);
+		}
+
+		sort(vecNumbers.begin(), vecNumbers.end(), &cmp);
+
+		auto iStart = vecNumbers.begin();
+		auto iEnd = vecNumbers.end() - 1;
+
+		while (iStart != iEnd) {
+			if ((*iStart).num + (*iEnd).num < target)
+				iStart++;
+			else if ((*iStart).num + (*iEnd).num > target)
+				iEnd--;
+			else {
+				result.push_back((*iStart).index);
+				result.push_back((*iEnd).index);
+				break;
 			}
 		}
 		return result;
 	}
 };
 
-
+//
 //int main()
 //{
 //	vector<int> ivec = { 0, 4, 3, 0 };
 //	Solution s;
 //	vector<int> result = s.twoSum(ivec, 0);
+//	for (auto i : result) {
+//		cout << i << endl;
+//	}
 //}
